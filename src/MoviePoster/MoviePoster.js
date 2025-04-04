6f1c5edac9476b861f11507251f6165b7dba8e35
Jaren Garman <jarengarman@protonmail.com>
2025-04-04 14:19:58 -0400

    style: Rework styling to match comp

--- src/MoviePoster/MoviePoster.js
+++ src/MoviePoster/MoviePoster.js
@@ -3,9 +3,8 @@ import './MoviePoster.css';
 function MoviePoster({ title, poster, vote_count }) {
   return (
     <section className='MoviePoster'>
-      <h3 className='poster_title'>{title}</h3>
       <img className='poster_image' src= {poster} alt="poster goes here"/>
-      <p>{vote_count}</p>
+      <p className='vote_count'>{vote_count}</p>
     </section>
   );
 }
