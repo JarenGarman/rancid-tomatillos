82c765c7428d7f3b046b18b6e4dfe7322c98b145
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-04-03 20:25:44 -0700

    displaying movie posters and vote_count

--- src/MoviePoster/MoviePoster.js
+++ src/MoviePoster/MoviePoster.js
@@ -1,9 +1,11 @@
 import './MoviePoster.css';
 
-function MoviePoster() {
+function MoviePoster({ title, poster, vote_count }) {
   return (
     <section className='MoviePoster'>
-      <p>This is a movie poster.</p>
+      <h3 className='poster_title'>{title}</h3>
+      <img className='poster_image' src= {poster} alt="poster goes here"/>
+      <p>{vote_count}</p>
     </section>
   );
 }
