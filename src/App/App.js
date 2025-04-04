4cb9f5a621666d868e58ce0dcf419d69949762a0
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-04-03 17:06:04 -0700

    importing moviePosters to App

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,26 @@
 import './App.css';
 import searchIcon from '../icons/search.png';
+import { useState, useEffect } from 'react';
+import moviePosters from './data/movie_posters';
+import MoviesContainer from '../Movi`esContainer/MoviesContainer';
+
+const posters_path = moviePosters
 
 // Example imports (for later):
-// import { useState, useEffect } from 'react';
-// import moviePosters from '../data/movie_posters';
 // import movieDetails from '../data/movie_details';
-// import MoviesContainer from '../MoviesContainer/MoviesContainer';
 
 function App() {
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {moviePosters},[])
+
   return (
     <main className='App'>
       <header>
         <h1>rancid tomatillos</h1>
       </header>
+      <MoviesContainer movies = {moviePosters}/>
     </main>
   );
 }
