d2fd662c51f183bb61f5f2ebccde31cc22277802
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-04-04 12:36:48 -0700

    adding click me for more

--- src/MoviePoster/MoviePoster.js
+++ src/MoviePoster/MoviePoster.js
@@ -5,6 +5,7 @@ function MoviePoster({ title, poster, vote_count }) {
     <section className='MoviePoster'>
       <img className='poster_image' src= {poster} alt="poster goes here"/>
       <p className='vote_count'>{vote_count}</p>
+      <div className="message">Click me for more!</div>
     </section>
   );
 }
