2b76dec8cd0c7f094d0ce810aa931a81565045d1
Jaren Garman <jarengarman@protonmail.com>
2025-04-07 17:33:33 -0400

    fix: Remove duplicate getMovies function

--- src/App/App.js
+++ src/App/App.js
@@ -1,28 +1,18 @@
 import { useEffect, useState } from 'react';
 import getMovies from '../apiCalls';
-import MoviesContainer from '../MoviesContainer/MoviesContainer';
 import MovieDetails from '../MovieDetails/MovieDetails';
+import MoviesContainer from '../MoviesContainer/MoviesContainer';
 import './App.css';
-// import movieDetails from '../data/movie_details';
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
 
-  function getMovies(){
-    setMovies(moviePosters || []) //we are simulating defining our network request in a separate function
-  }
-
-  useEffect(() => getMovies(),[])
-
-  const vote = (id, modifier = 1) => {
-    const movie = movies.filter(movie => movie.id === id)[0]
-    movie.vote_count += (1 * modifier)
-  const [movies, setMovies] = useState([]);
-
   useEffect(() => {
     getMovies(setMovies)
   }, [])
+
+  useEffect(() => getMovies(setMovies),[])
 
   const vote = (id, votes_change) => {
     const movie = movies.find(movie => movie.id === id)
@@ -36,11 +26,11 @@ function App() {
         <h1>rancid tomatillos</h1>
       </header>
       {
-        selectedMovie ? 
-          <MovieDetails movie= {selectedMovie} goBack={() => setSelectedMovie(null)} /> 
-          : <MoviesContainer 
-              movies={movies} 
-              vote={vote} 
+        selectedMovie ?
+          <MovieDetails movie= {selectedMovie} goBack={() => setSelectedMovie(null)} />
+          : <MoviesContainer
+              movies={movies}
+              vote={vote}
               selectMovie={setSelectedMovie} />
       }
       <MoviesContainer movies = {movies} vote = {vote}/>
