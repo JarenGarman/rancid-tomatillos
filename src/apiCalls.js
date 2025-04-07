4873c578d44f175d590ba847529841a22c0b190a
Jaren Garman <jarengarman@protonmail.com>
2025-04-07 19:12:47 -0400

    refactor: Extract base URL to global variable

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,8 @@
+const baseURL = "https://rancid-tomatillos-api-ce4a3879078e.herokuapp.com/api/v1"
+
 function getMovies(setMovies) {
-  fetch("https://rancid-tomatillos-api-ce4a3879078e.herokuapp.com/api/v1/movies")
+  const url = baseURL + "/movies"
+  fetch(url)
     .then(response => response.json())
     .then(data => setMovies([...data]))
     .catch(e => console.log(e));
