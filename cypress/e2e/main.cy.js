288651ee66f588b3cfe619fac4f06869c4bf1cbf
Jaren Garman <jarengarman@protonmail.com>
2025-04-08 16:40:24 -0400

    test: Add test for home button functionality

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -71,4 +71,43 @@ describe("Main Page", () => {
         "Batman raises the stakes in his war on crime. With the help of Lt. Jim Gordon and District Attorney Harvey Dent, Batman sets out to dismantle the remaining criminal organizations that plague the streets. The partnership proves to be effective, but they soon find themselves prey to a reign of chaos unleashed by a rising criminal mastermind known to the terrified citizens of Gotham as the Joker."
       );
   });
+
+  it("navigates home from movie details when home button clicked", () => {
+    cy.intercept(
+      "GET",
+      "https://rancid-tomatillos-api-ce4a3879078e.herokuapp.com/api/v1/movies/155",
+      details
+    )
+      .get(".movies-container")
+      .find(".MoviePoster")
+      .first()
+      .click()
+      .get("header button")
+      .click()
+      .get("h1")
+      .contains("rancid tomatillos")
+      .get(".movies-container")
+      .find(".MoviePoster")
+      .should("have.length", 4)
+      .get(".poster_image")
+      .first()
+      .should(
+        "have.attr",
+        "src",
+        "https://image.tmdb.org/t/p/original//qJ2tW6WMUDux911r6m7haRef0WH.jpg"
+      )
+      .get(".poster_image")
+      .last()
+      .should(
+        "have.attr",
+        "src",
+        "https://image.tmdb.org/t/p/original//d5iIlFn5s0ImszYzBPb8JPIfbXD.jpg"
+      )
+      .get(".vote_count")
+      .first()
+      .contains("32544")
+      .get(".vote_count")
+      .last()
+      .contains("27642");
+  });
 });
