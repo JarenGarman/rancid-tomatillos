1ae48d4019bfc4e1a070e79063a7319d7734d7bc
Jaren Garman <jarengarman@protonmail.com>
2025-04-08 15:39:31 -0400

    test: Flesh out main page test to display titles and stubbed movies

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -1,12 +1,40 @@
-// Mock data to use for testing:
-// import posters from '../fixtures/movie_posters.json' (we've added mock data to this file for you!)
+import posters from "../fixtures/movie_posters.json";
 // import details from '../fixtures/movie_details.json' (you will need to add your own mock data to this file!)
 
-describe('Main Page', () => {
-  it('displays title on page load', () => {
-    // hint: you'll want to add an intercept here if you are making a network request on page load!
-    cy.visit('http://localhost:3000/')
-    .get('h1')
-    .contains('rancid tomatillos')
-  })
-})
+describe("Main Page", () => {
+  beforeEach(() => {
+    cy.intercept(
+      "GET",
+      "https://rancid-tomatillos-api-ce4a3879078e.herokuapp.com/api/v1/movies",
+      posters
+    );
+    cy.visit("http://localhost:3000/");
+  });
+  it("displays title and movies on page load", () => {
+    cy.get("h1")
+      .contains("rancid tomatillos")
+      .get(".movies-container")
+      .find(".MoviePoster")
+      .should("have.length", 4)
+      .get(".poster_image")
+      .first()
+      .should(
+        "have.attr",
+        "src",
+        "https://image.tmdb.org/t/p/original//qJ2tW6WMUDux911r6m7haRef0WH.jpg"
+      )
+      .get(".poster_image")
+      .last()
+      .should(
+        "have.attr",
+        "src",
+        "https://image.tmdb.org/t/p/original//d5iIlFn5s0ImszYzBPb8JPIfbXD.jpg"
+      )
+      .get(".vote_count")
+      .first()
+      .contains("32544")
+      .get(".vote_count")
+      .last()
+      .contains("27642");
+  });
+});
