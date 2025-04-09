dae17dbfd39a33d1ce9eec1265ed40e14c18f251
Jaren Garman <jarengarman@protonmail.com>
2025-04-09 15:04:02 -0400

    refactor: Clean up styling and remove setMovies from apiCalls

--- src/MoviePoster/MoviePoster.js
+++ src/MoviePoster/MoviePoster.js
@@ -1,19 +1,30 @@
-import './MoviePoster.css';
+import "./MoviePoster.css";
 
-function MoviePoster({ id, poster, vote_count, vote, getMovie, setSelectedMovie }) {
+function MoviePoster({
+  id,
+  poster,
+  vote_count,
+  vote,
+  getMovie,
+  setSelectedMovie,
+}) {
   return (
-    <section className='MoviePoster'>
+    <section className="MoviePoster">
       <img
-        className='poster_image'
-        src= {poster}
+        className="poster_image"
+        src={poster}
         alt="poster goes here"
         onClick={() => getMovie(id, setSelectedMovie)}
-        />
+      />
       <div className="message">Click me for more!</div>
-      <div className='votes'>
-        <button className='upvote' onClick={() => vote(id, 1)}>↑</button>
-        <p className='vote_count'>{vote_count}</p>
-        <button className='downvote' onClick={() => vote(id, -1)}>↓</button>
+      <div className="votes">
+        <button className="upvote" onClick={() => vote(id, "up")}>
+          ↑
+        </button>
+        <p className="vote_count">{vote_count}</p>
+        <button className="downvote" onClick={() => vote(id, "down")}>
+          ↓
+        </button>
       </div>
     </section>
   );
