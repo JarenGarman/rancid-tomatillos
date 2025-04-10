97c0993a8caa57aba86fd66b3fcb570cf5b625f8
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-04-10 10:37:42 -0700

    updating Cypress test genere

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -67,7 +67,7 @@ describe("Main Page", () => {
       .should('contain',"Drama")
       .get("h3")
       .last()
-      .contains("Thriller")
+      .should('contain',"Thriller")
       .get("p")
       .should('contain',
         "Batman raises the stakes in his war on crime. With the help of Lt. Jim Gordon and District Attorney Harvey Dent, Batman sets out to dismantle the remaining criminal organizations that plague the streets. The partnership proves to be effective, but they soon find themselves prey to a reign of chaos unleashed by a rising criminal mastermind known to the terrified citizens of Gotham as the Joker."
